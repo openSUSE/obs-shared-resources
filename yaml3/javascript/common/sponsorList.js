657fdac1a3ad0c25b90fd1eb8090d451e8f4d32c
Robert Lihm <rlihm@suse.de>
2009-09-01 13:32:50 +0000

    fixed broken AMD link

--- yaml3/javascript/common/sponsorList.js
+++ yaml3/javascript/common/sponsorList.js
@@ -12,7 +12,7 @@ var SPONSORS = new Array();
 /**
 ** Links and images
 **/
-SPONSORS[0] = new Array("http://en.opensuse.org/Sponsors/AMD","http://files.test.opensuse.org/opensuse/en/c/cc/Amd.jpg");
+SPONSORS[0] = new Array("http://en.opensuse.org/Sponsors/AMD","http://files.opensuse.org/opensuse/en/c/cc/Amd.jpg");
 SPONSORS[1] = new Array("http://en.opensuse.org/Sponsors/IP_Exchange","http://files.opensuse.org/opensuse/en/b/b6/Ip-exchange.gif");
 SPONSORS[2] = new Array("http://en.opensuse.org/Sponsors","http://files.opensuse.org/opensuse/en/f/fc/B1-systems.jpg");
 
