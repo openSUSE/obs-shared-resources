0512d8058b8dbdd47ebd7a716296fb54a805875e
Sascha Peilicke <saschpe@suse.de>
2011-03-22 16:16:49 +0100

    Revert "use ssl based on the used protocol and not the port"
    This reverts commit 4265bc2b76d895a63c58b0d4cb06dd9163a0ffca.
    Doesn't solve the issue, makes it worse.

--- buildservice/lib/activexml/transport.rb
+++ buildservice/lib/activexml/transport.rb
@@ -475,7 +475,8 @@ module ActiveXML
           keepalive = true
           if not @http
             @http = Net::HTTP.new(url.host, url.port)
-            @http.use_ssl = true if url.scheme == "https"
+            # FIXME: we should get the protocol here instead of depending on the port
+            @http.use_ssl = true if url.port == 443
             @http.start
           end
           @http.read_timeout = opt[:timeout]
